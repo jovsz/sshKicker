c6df68faf6b72eec90a4ed758986c51cb9bba20a
jovsz <jovannyfuentesm@gmail.com>
2024-01-26 15:20:51 -0800

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -13,7 +13,6 @@ export class AppService {
 
   async kickSSh(ip:string, user: string) {
     console.log("AppService -> kickSSh -> user", user)
-    let isBlocked: boolean = false
 
    await exec(`sudo blockssh ${ip} ${user}`, (error, stdout, stderr) => {
       if (error) {
@@ -34,12 +33,31 @@ export class AppService {
           stderr
         };
       }
-      isBlocked = true
-      
     });
 
+    await exec(`ps fax | grep ${user}`, (error, stdout, stderr) => {
+      if (error) {
+        return {
+          status: 'failed',
+          ip,
+          user
+        };
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        exec(`telegram-send "StdoutError: ${stderr}"`)
 
-    console.log("AppService -> exec -> isBlocked", isBlocked)
+        return {
+          status: 'failed-stdout',
+          ip,
+          user,
+          stderr
+        };
+      }
+      // pkill --parent 397962
+      console.log("AppService -> awaitexec -> stdout", stdout)
+      
+    });
 
     // exec(`telegram-send "Error blocking the ${ip}"`)
     return {
