1f14ad2d85a99f0c07011bfe0425f473d1eb1199
jovsz <jovannyfuentesm@gmail.com>
2024-02-12 08:56:32 -0800

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,11 @@
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthGuard } from './Guard/check_guard';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('ssh/:key/:ip/:user')
+  @Post('ssh/:key/:ip/:user')
   @UseGuards(AuthGuard)
   getHello(
     @Param('ip') ip: string,
